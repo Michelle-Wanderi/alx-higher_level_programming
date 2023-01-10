00f7e1c7856d7240aa1b214471157edbbdf61d48
Michelle-Wanderi <michellewangari12@gmail.com>
2023-01-10 10:48:12 -0800

    fixed indentation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
+
 
 """
-	Returns an object represented by a json string
-"""
+           Returns an object represented by a json string
+   """
 import json
-	
+   
+         
 def from_json_string(my_str):
-
-"""
-	Args : my_str - the json string to be converted to an object
-	
-	Returns : an object parsed for the json string
-"""
-return json.loads(my_str)
+  
+  """
+          Args : my_str - the json string to be converted to an object
+          
+          Returns : an object parsed for the json string
+ """
+  return json.loads(my_str)
