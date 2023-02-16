797810616231f268f58ac45c9fdb1ec0e8fec6e7
Michelle-Wanderi <michellewangari12@gmail.com>
2023-02-16 05:34:27 +0300

    fixed

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
--- Create a user user_0d_1 with the password user_0d_1_pwd
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES 
-ON *.*
-TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- Creates the table unique_id
+CREATE TABLE IF NOT EXISTS `unique_id` (
+    `id`   INT          DEFAULT 1 UNIQUE,
+    `name` VARCHAR(256)
+);
